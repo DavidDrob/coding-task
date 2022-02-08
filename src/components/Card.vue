583b8ddfdac5112f67e1ff995b7d734c316a0b4e
daviddrob <david.drobnak26@gmail.com>
2022-02-08 18:22:14 +0100

    feat: bind data from pokeapi to main component

--- src/components/Card.vue
+++ src/components/Card.vue
@@ -1,25 +1,28 @@
 <template>
-  <a href="https://www.google.at" class="cursor-pointer">
+  <a :href="link" class="cursor-pointer">
     <div
-      class="grid place-items-center bg-slate-900 py-6 text-white rounded-xl"
+      class="
+        grid
+        place-items-center
+        bg-slate-900
+        h-52
+        pb-4
+        text-white
+        rounded-xl
+      "
     >
-      <img src="../assets/logo.png" alt="image" class="w-20" />
+      <img :src="image" alt="image" class="w-24" />
       <p class="font-bold">
-        {{ title }}
+        {{ name }}
       </p>
-      <p
-        class="
-          text-center
-          w-9/12
-          pt-2
-          block
-          overflow-ellipsis
-          break-words
-          overflow-hidden
-          max-h-14
-        "
-      >
-        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum nam
+      <p class="text-center w-7/12 pt-2">
+        <!-- Display a comma after every ability or a dot after the last ability -->
+        {{ name }}'s abilities are:
+        <span v-for="(ability, index) in abilities" :key="index"
+          >{{ ability.ability.name
+          }}<span v-if="abilities.length - 1 != index">, </span
+          ><span v-else>.</span>
+        </span>
       </p>
     </div>
   </a>
@@ -27,6 +30,15 @@
 
 <script>
 export default {
-  props: ["title"],
+  props: ["title", "image", "link", "abilities"],
+  data() {
+    return {
+      name: "",
+    };
+  },
+  mounted() {
+    // Capitalize the first letter of the name
+    this.name = this.title.charAt(0).toUpperCase() + this.title.slice(1);
+  },
 };
 </script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -3,20 +3,51 @@
     <main
       class="w-5/6 m-auto grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
     >
-      <Card title="1" />
-      <Card title="2" />
-      <Card title="3" />
+      <Card
+        v-for="pokemon in pokemons"
+        :key="pokemon.id"
+        :title="pokemon.name"
+        :image="pokemon.image"
+        :link="pokemon.link"
+        :abilities="pokemon.abilities"
+      />
     </main>
   </div>
 </template>
 
 <script>
 import Card from "./Card.vue";
+import axios from "axios";
 
 export default {
   name: "Main",
+  data() {
+    return {
+      pokemons: [],
+    };
+  },
   components: {
     Card,
+  },
+  mounted() {
+    // Request first 22 Pokemon's data and push them to the `pokemons` array
+    for (let i = 1; i < 22; i++) {
+      axios.get(`https://pokeapi.co/api/v2/pokemon/${i}`).then((r) => {
+        const id = r.data.id;
+        const name = r.data.name;
+        const image = r.data.sprites.front_default;
+        const abilities = r.data.abilities;
+        const link = "https://www.google.at/search?q=" + r.data.name;
+
+        this.pokemons.push({
+          id,
+          name,
+          image,
+          abilities,
+          link,
+        });
+      });
+    }
   },
 };
 </script>
