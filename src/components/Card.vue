8e91164046fc7b02fd6162194ab8b10792fd8fac
daviddrob <david.drobnak26@gmail.com>
2022-02-08 17:01:44 +0100

    feat: add responsive main component

---
+++ src/components/Card.vue
@@ -0,0 +1,32 @@
+<template>
+  <a href="https://www.google.at" class="cursor-pointer">
+    <div
+      class="grid place-items-center bg-slate-900 py-6 text-white rounded-xl"
+    >
+      <img src="../assets/logo.png" alt="image" class="w-20" />
+      <p class="font-bold">
+        {{ title }}
+      </p>
+      <p
+        class="
+          text-center
+          w-9/12
+          pt-2
+          block
+          overflow-ellipsis
+          break-words
+          overflow-hidden
+          max-h-14
+        "
+      >
+        Lorem, ipsum dolor sit amet consectetur adipisicing elit. Rerum nam
+      </p>
+    </div>
+  </a>
+</template>
+
+<script>
+export default {
+  props: ["title"],
+};
+</script>
--- src/components/Main.vue
+++ src/components/Main.vue
@@ -1,13 +1,22 @@
 <template>
   <div class="text-black py-4">
-    <main class="w-5/6 m-auto flex items-center justify-between">
-      <h1>Main</h1>
+    <main
+      class="w-5/6 m-auto grid gap-4 grid-cols-1 md:grid-cols-2 lg:grid-cols-3"
+    >
+      <Card title="1" />
+      <Card title="2" />
+      <Card title="3" />
     </main>
   </div>
 </template>
 
 <script>
+import Card from "./Card.vue";
+
 export default {
   name: "Main",
+  components: {
+    Card,
+  },
 };
 </script>
